a99ae350b585c18def4c4e9d9dded35a45075780
gdeepanshu327-debug <gdeepanshu327@gmail.com>
2026-06-04 09:49:03 +0530

    add two digits in a number logic program

---
+++ DS/adddigits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int addDigits(int num) {
+    while(num >= 10){
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    return num;
+}
+int main(){
+    int num ;
+    cout<<"Enter a number: ";
+    cin>>num;
+    
+    cout<<addDigits(num)<<endl;
+    return 0;
+}
