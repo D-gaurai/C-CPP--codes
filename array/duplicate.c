7f393ea8f4863b532db80bc58362bf568ef9554e
Deepanshu Gaur <gdeepanshu327@gmail.com>
2026-06-04 20:28:41 +0530

    Implement duplicate element detection in array
    Refactor duplicate detection logic and improve output formatting.

--- array/duplicate.c
+++ array/duplicate.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+//basic code native approach  not included all the edge cases
+/*#include<stdio.h>
 int main(){
     int arr[7] = {1,2,3,4,7,5,7};
     for(int i=0;i<=6;i++){
@@ -10,5 +11,27 @@ int main(){
         }
     }
     return 0;
+}*/
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,7,5,7,7};
+int n=sizeof(arr)/sizeof(arr[0]);
+printf("Duplicate elements are:\n");
+for(int i=0;i<n;i++){
+int duplicate=0;
+for(int k=0;k<i;k++){
+if(arr[i]==arr[k]){
+duplicates=1;
+break;}
+}
+if(duplicate){
+continue;}
+for(int j=i+1;j<n;j++){
+if(arr[i]==arr[j]){
+printf("%d is a duplicate element\n",arr[i]);
+break;}
+}
+}
+return 0;}
 
-    }
+    
