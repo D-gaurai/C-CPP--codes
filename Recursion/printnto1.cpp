d2b55dcc5411617d202faa75fa0674ee2615f786
gdeepanshu327-debug <gdeepanshu327@gmail.com>
2026-06-14 09:58:25 +0530

    print number from n to 1

---
+++ Recursion/printnto1.cpp
@@ -0,0 +1,18 @@
+//printing number from n to 1 using recursion
+#include<iostream>
+using namespace std;
+void print(int N){
+    //base case
+    if(N==0){
+        return;
+    }
+    cout<<N<<endl;
+    print(N-1);
+}
+int main(){
+    int N;
+    cout<<"Enter the value of N: ";
+    cin>>N;
+    print(N);
+    return 0;
+}
