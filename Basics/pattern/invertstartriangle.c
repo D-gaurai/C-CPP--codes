560198c24d4039099de0d018f3a4934aae927d43
Deepanshu Gaur <gdeepanshu327@gmail.com>
2026-06-17 12:37:58 +0530

    Fix star printing in inverted triangle pattern

--- Basics/pattern/invertstartriangle.c
+++ Basics/pattern/invertstartriangle.c
@@ -20,9 +20,10 @@ int main(){
   int a=n;
   for(int i=1;i<=n;i++){
     for(int j=1;j<=a;j++){
-        printf("*");
+        printf("*"); 
     }
     a--;
+    
     printf("\n");
   }
   return 0;}
