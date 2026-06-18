bba748da92a3e180a3dee4a21ac24ee6184926cb
Deepanshu Gaur <gdeepanshu327@gmail.com>
2026-06-18 20:12:57 +0530

    Add reverse alphabet triangle program in C
    This program prints a reverse alphabet triangle based on the number of rows input by the user.

---
+++ Basics/pattern/reversealphabettriangle.c
@@ -0,0 +1,20 @@
+//print reverse alphabet triangle
+
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter the no of rows:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        int a=1;
+        for(j=1;j<=i;j++){
+            int d=a+64;
+            char ch=(char)d;
+            printf("%c ",ch);
+            a++;
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
