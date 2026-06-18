5ca8528794770311f049d14845f1efae13765ed9
Deepanshu Gaur <gdeepanshu327@gmail.com>
2026-06-18 20:12:00 +0530

    Add alphabet triangle pattern program in C

---
+++ Basics/pattern/alphabettriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter the no of rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        int a=1;
+        for(j=1;j<=i;j++){
+           int  d=a+64;
+            char ch=(char)d;
+            printf("%c ",ch);
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
